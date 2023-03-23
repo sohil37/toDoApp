d0d5cf06ea09b86cc6e1e39ad84747a7f97ca188
sohil37 <53380122+sohil37@users.noreply.github.com>
2023-03-23 20:45:35 +0530

    removed default input text

--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -5,7 +5,7 @@ class AddToDo extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { formInput: "sohil" };
+    this.state = { formInput: "" };
   }
 
   changeFormInput = (e) => {
